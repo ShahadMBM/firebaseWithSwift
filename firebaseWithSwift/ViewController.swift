d061c2b2f5f52ba8e7a3a075824492d257272a20
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-04-16 21:41:38 +0300

    Initial Commit

---
+++ firebaseWithSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseWithSwift
+//
+//  Created by Shahad on 15/09/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
